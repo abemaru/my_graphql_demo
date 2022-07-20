de17e382af573931a93387d61de2fdfbf461796f
L3dlightening <ch4nge.lightening@gmail.com>
2022-07-21 01:09:51 +0900

    データ操作追加

--- graphql_sample/schema.py
+++ graphql_sample/schema.py
@@ -15,8 +15,57 @@ class UserType(DjangoObjectType):
 class ItemType(DjangoObjectType):
     class Meta:
         model = Item
-        filter_fields = ['name', 'description', 'brand']
+        filter_fields = ['name', 'description', 'brand', 'user__username']
         interfaces = (relay.Node,)
+
+
+class CreateItem(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        description = graphene.String()
+        brand = graphene.String()
+        user = graphene.Int()
+
+    item = graphene.Field(ItemType)
+
+    def mutate(root, info, name, description, brand, user):
+        user = User.objects.get(pk=user)
+        item = Item(name=name, description=description, brand=brand, user=user)
+        item.save()
+        return CreateItem(item=item)
+
+
+class DeleteItem(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+
+    ok = graphene.Boolean()
+
+    def mutate(root, info, name):
+        item = Item.objects.filter(name=name)
+        item.delete()
+        return DeleteItem(ok=True)
+
+
+class UpdateItemUser(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        user = graphene.Int()
+
+    ok = graphene.Boolean()
+
+    def mutate(root, info, name, user):
+        obj, _ = Item.objects.get_or_create(
+            name=name,
+            user_id=user
+        )
+        return UpdateItemUser(ok=True)
+
+
+class MyMutation(graphene.ObjectType):
+    create_item = CreateItem.Field()
+    delete_item = DeleteItem.Field()
+    update_item_user = UpdateItemUser.Field()
 
 
 class Query(graphene.ObjectType):
@@ -33,4 +82,4 @@ class Query(graphene.ObjectType):
         return Item.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=MyMutation)
